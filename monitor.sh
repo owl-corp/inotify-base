f9d51f09dacfa9c0feeab3794815d387f4139596
Joe Banks <joe@jb3.dev>
2024-06-09 19:02:48 +0100

    Correct conditionals for script delay

--- monitor.sh
+++ monitor.sh
@@ -17,7 +17,7 @@ else
     fi;
 fi;
 
-if [ -n "$WATCH_EVENTS" ]; then
+if [ -n "$INOTIFY_WATCH_EVENTS" ]; then
   ADDITIONAL_ARGS="-e $INOTIFY_WATCH_EVENTS"
 else
   ADDITIONAL_ARGS=""
@@ -30,7 +30,7 @@ else
 fi
 
 while inotifywait $ADDITIONAL_ARGS -r $WATCH_DIR; do
-    if [ -z ${INOTIFY_HOOK_DELAY+x} ]; then
+    if [ ! -z ${INOTIFY_HOOK_DELAY+x} ]; then
         echo "Waiting $INOTIFY_HOOK_DELAY until executing hook..."
         sleep $INOTIFY_HOOK_DELAY
     fi
